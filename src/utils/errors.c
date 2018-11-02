4d2e5b83754ab30f6819bd37eb8b4d37828a2026
Paul BIE <pbie@e3r13p5.42.fr>
2018-11-02 20:21:41 +0100

    [Fix] - All Caps Error

--- src/utils/errors.c
+++ src/utils/errors.c
@@ -6,7 +6,7 @@
 /*   By: pbie <pbie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 15:35:19 by pbie              #+#    #+#             */
-/*   Updated: 2018/11/01 13:55:00 by pbie             ###   ########.fr       */
+/*   Updated: 2018/11/02 20:21:39 by pbie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	path_error(const char *msg, t_data *data, t_bfs *bfs)
 {
 	if (!ft_strcmp(msg, "end"))
-		ft_exit("Error Occured: No path from start to end rooms");
+		ft_exit("ERROR Occured: No path from start to end rooms");
 }
 
 void	empty_line(char *line)
@@ -23,7 +23,7 @@ void	empty_line(char *line)
 	if (ft_only_white(line))
 	{
 		free(line);
-		ft_exit("Error Occurred: Empty Line Given!");
+		ft_exit("ERROR Occurred: Empty Line Given!");
 	}
 	free(line);
 }
@@ -31,27 +31,27 @@ void	empty_line(char *line)
 void	error(const char *msg, t_data *data, char *line)
 {
 	if (!ft_strcmp(msg, "empty"))
-		ft_exit("Error Occurred: Empty Map!");
+		ft_exit("ERROR Occurred: Empty Map!");
 	if (!ft_strcmp(msg, "ants"))
-		ft_exit("Error Occurred: Not Enough Ants!");
+		ft_exit("ERROR Occurred: Not Enough Ants!");
 	if (!ft_strcmp(msg, "start"))
-		ft_exit("Error Occurred: No Start Command!");
+		ft_exit("ERROR Occurred: No Start Command!");
 	if (!ft_strcmp(msg, "startroom"))
-		ft_exit("Error Occurred: No Start Room Given!");
+		ft_exit("ERROR Occurred: No Start Room Given!");
 	if (!ft_strcmp(msg, "end"))
-		ft_exit("Error Occurred: No End Command!");
+		ft_exit("ERROR Occurred: No End Command!");
 	if (!ft_strcmp(msg, "endroom"))
-		ft_exit("Error Occurred: No End Room Given!");
+		ft_exit("ERROR Occurred: No End Room Given!");
 	if (!ft_strcmp(msg, "rooms"))
-		ft_exit("Error Occurred: No Rooms Given!");
+		ft_exit("ERROR Occurred: No Rooms Given!");
 	if (!ft_strcmp(msg, "room"))
 	{
 		empty_line(line);
-		ft_exit("Error Occurred: Invalid Room Given!");
+		ft_exit("ERROR Occurred: Invalid Room Given!");
 	}
 	if (!ft_strcmp(msg, "link"))
 	{
 		empty_line(line);
-		ft_exit("Error Occurred: Invalid Link Given!");
+		ft_exit("ERROR Occurred: Invalid Link Given!");
 	}
 }
